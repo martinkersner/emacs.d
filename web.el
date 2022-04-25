b2ed6411773c7bf7446a046fac9feae7e6bd9f15
Martin Kersner <m.kersner@gmail.com>
2022-04-25 11:35:52 +0900

    Remove vue related packages and add typescript

--- web.el
+++ web.el
@@ -1,19 +1,26 @@
-(use-package vue-mode
-  :ensure t)
-
-(add-hook 'mmm-mode-hook
-          (lambda ()
-            (set-face-background 'mmm-default-submode-face nil)))
-
-(setq mmm-js-mode-enter-hook (lambda () (setq syntax-ppss-table nil)))
-(setq mmm-typescript-mode-enter-hook (lambda () (setq syntax-ppss-table nil)))
-
-(setq js-indent-level 2)
-
-(use-package vue-html-mode
-  :ensure t)
-
 ;;; html+css autocomplete
 (use-package emmet-mode
   :ensure t)
 (setq emmet-move-cursor-between-quotes t)
+
+;;;; Typescript
+;;;; https://github.com/ananthakumaran/tide
+(defun setup-tide-mode ()
+  (interactive)
+  (tide-setup)
+  (flycheck-mode +1)
+  (setq flycheck-check-syntax-automatically '(save mode-enabled))
+  (eldoc-mode +1)
+  (tide-hl-identifier-mode +1)
+  ;; company is an optional dependency. You have to
+  ;; install it separately via package-install
+  ;; `M-x package-install [ret] company`
+  (company-mode +1))
+
+;; aligns annotation to the right hand side
+(setq company-tooltip-align-annotations t)
+
+;; formats the buffer before saving
+(add-hook 'before-save-hook 'tide-format-before-save)
+
+(add-hook 'typescript-mode-hook #'setup-tide-mode)
