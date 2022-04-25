1fd7471b2f4412ee77d6cf9e66f983b505ca84ec
Martin Kersner <m.kersner@gmail.com>
2022-04-25 11:35:29 +0900

    Cleanup python packages

--- py.el
+++ py.el
@@ -1,30 +1,19 @@
 ;;; Python --- Packages related to Python development
 
-;; Virtualenvwrapper ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; (use-package virtualenvwrapper
-  ;; :ensure t)
-;; if you want interactive shell support
-(venv-initialize-interactive-shells)
-;; if you want eshell support
-(venv-initialize-eshell)
+(pyvenv-workon "emacs")
 
-
-;; Elpy ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Elpy
 (use-package elpy
   :ensure t)
 (elpy-enable)
-(setq python-shell-completion-native-enable nil)
-(setq elpy-shell-echo-input nil)
-(setq elpy-rpc-backend "jedi")
-;; (define-key python-mode-map (kbd "C-]") #'elpy-goto-definition)
-;; (elpy-set-test-runner #'elpy-test-pytest-runner)
 
-;; load python packages
-(use-package pyvenv
+;; isort
+(use-package py-isort
   :ensure t)
-(pyvenv-workon "emacs")
+(require 'py-isort)
 
-;; Enable autopep8
-(use-package py-autopep8
-  :ensure t)
-(add-hook 'elpy-mode-hook 'py-autopep8-enable-on-save)
+(add-hook 'elpy-mode-hook
+          (lambda ()
+            (add-hook 'before-save-hook 'py-isort-before-save)
+            (add-hook 'before-save-hook 'elpy-black-fix-code nil t)
+            ))
