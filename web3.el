71b70c37b02ec6f428e012db02e7f79e5aafd3a1
Martin Kersner <martin@bisonai.com>
2022-08-16 09:54:08 +0900

    feat: Add flymake for solidity and graphql-mode

--- web3.el
+++ web3.el
@@ -1,2 +1,10 @@
 (use-package solidity-mode
   :ensure t)
+
+(use-package flymake-solidity
+  :ensure t)
+(add-hook 'solidity-mode-hook 'flymake-solidity-load)
+(setq flymake-solidity-executable "/opt/homebrew/bin/solcjs")
+
+(use-package graphql-mode
+  :ensure t)
