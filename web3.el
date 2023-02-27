b71247833794703f35b3fe001ae074bb4709e444
Martin Kersner <martin@bisonai.com>
2023-02-27 22:12:37 +0900

    fix: stop web3

--- web3.el
+++ web3.el
@@ -1,10 +1,26 @@
-(use-package solidity-mode
-  :ensure t)
+;; (setq solidity-comment-style 'slash)
 
-(use-package flymake-solidity
-  :ensure t)
-(add-hook 'solidity-mode-hook 'flymake-solidity-load)
-(setq flymake-solidity-executable "/opt/homebrew/bin/solcjs")
+;; (setq solidity-solc-path "/opt/homebrew/bin/solcjs")
+;; (setq solidity-flycheck-solc-checker-active t)
 
-(use-package graphql-mode
-  :ensure t)
+;; (use-package solidity-flycheck
+;;   :ensure t)
+;; (require 'solidity-flycheck)
+
+;; (use-package solidity-mode
+;;   :ensure t)
+;; (require 'solidity-mode)
+
+;; (add-to-list 'load-path "/Users/martin/git/emacs-solidity/")
+;; (load "solidity-common.el")
+;; (load "solidity-flycheck.el")
+;; (load "solidity-mode.el")
+
+;; (use-package solidity-flycheck
+;;     :hook solidity-mode
+;;     :config
+;;     (setq solidity-solc-path "/opt/homebrew/bin/solcjs")
+;;     )
+
+;; (use-package graphql-mode
+;;   :ensure t)
