7efdfbffb52dbcf0d382b333cd4b1c8d1cf12747
Martin Kersner <martin@bisonai.com>
2023-10-02 10:36:10 +0900

    feat: forge fmt on save in solidity-mode

--- web3.el
+++ web3.el
@@ -25,11 +25,22 @@
 (setq xref-prompt-for-identifier nil)
 (setq xref-auto-jump-to-first-xref nil)
 
-(use-package company-solidity
-  :ensure t)
-(require 'company-solidity)
-(add-hook 'solidity-mode-hook
-	(lambda ()
-	(set (make-local-variable 'company-backends)
-		(append '((company-solidity company-capf company-dabbrev-code))
-			company-backends))))
+;; (use-package company-solidity
+;;   :ensure t)
+;; (require 'company-solidity)
+;; (add-hook 'solidity-mode-hook
+;; 	(lambda ()
+;; 	  (set (make-local-variable 'company-backends)
+;;                (append '((company-solidity company-capf company-dabbrev-code))
+;; 			company-backends))))
+;; (add-hook 'after-init-hook 'global-company-mode)
+
+
+(defun format-solidity ()
+  (when (eq major-mode 'solidity-mode)
+    (shell-command (format "forge fmt %s" buffer-file-name))
+    (revert-buffer :ignore-auto :noconfirm)
+    )
+  )
+
+(add-hook 'before-save-hook #'format-solidity)
